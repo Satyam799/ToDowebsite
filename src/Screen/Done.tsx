79633536b5d097616c562bdae5426c94f328e1ca
Satyam799 <satyamtripathisvp@gmail.com>
2024-12-26 00:59:24 +0530

    update

--- src/Screen/Done.tsx
+++ src/Screen/Done.tsx
@@ -1,7 +1,7 @@
 import List from "../Comnponent/List";
 import { useDispatch, useSelector } from "react-redux";
 import Modal from "../Comnponent/Modal";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { TodostateAll } from "../Store/Todoslice";
 import { useNavigate } from "react-router-dom";
 import { Rootstate } from "./All";
@@ -14,7 +14,7 @@ export const Done :React.FC =()=>{
   const [text,settext]=useState('')
   const dispath=useDispatch()
   const navigate=useNavigate()
-
+  const value=useRef<HTMLTextAreaElement>(null)
 
   function handelsubmit (e:React.FormEvent<HTMLFormElement>|React.MouseEvent<HTMLButtonElement>){
     e.preventDefault()
@@ -29,7 +29,7 @@ export const Done :React.FC =()=>{
   return (
     <>
       <div className={`Modal ${hide ? '' :'hide'}`}>
-        <Modal settext={settext} text={text} sethide={sethide} handelsubmit={handelsubmit}/>
+        <Modal settext={settext} text={text} value={value} sethide={sethide} handelsubmit={handelsubmit}/>
       </div>
       <div className={`list ${!hide ? '' :'hide'}`}>
         {Done?.map((el,i) => {
--- src/Screen/Inprogress.tsx
+++ src/Screen/Inprogress.tsx
@@ -1,7 +1,7 @@
 import List from "../Comnponent/List";
 import { useDispatch, useSelector } from "react-redux";
 import Modal from "../Comnponent/Modal";
-import { useRef, useState } from "react";
+import {  useRef, useState } from "react";
 import { TodostateAll } from "../Store/Todoslice";
 import { useNavigate } from "react-router-dom";
 import { Rootstate } from "./All";
@@ -13,9 +13,11 @@ export const Inprogress :React.FC =()=>{
   const [text,settext]=useState('')
   const dispath=useDispatch()
   const navigate=useNavigate()
-
+  const value=useRef<HTMLTextAreaElement>(null)
   function handelsubmit (e:React.FormEvent<HTMLFormElement>|React.MouseEvent<HTMLButtonElement>){
     e.preventDefault()
+    if(text==='')return value.current?.focus() 
+
     if(text==='')return 
 
     dispath(TodostateAll(text))
@@ -27,7 +29,7 @@ export const Inprogress :React.FC =()=>{
   return (
     <>
       <div className={`Modal ${hide ? '' :'hide'}`}>
-        <Modal settext={settext} text={text} sethide={sethide} handelsubmit={handelsubmit} />
+        <Modal settext={settext} value={value} text={text} sethide={sethide} handelsubmit={handelsubmit} />
       </div>
       <div className={`list ${!hide ? '' :'hide'}`}>
         {inprogress?.map((el:string,i:number) => {
